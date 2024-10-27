4eb4a10a6a3ece53f42b60d9cf99178e8266227c
msilis <mikssilis@gmail.com>
2024-10-27 20:30:42 +0000

    chore: initial sqlite and drizzle setup

--- manage_backend/index.mjs
+++ manage_backend/index.mjs
@@ -1,11 +1,13 @@
 import express from "express";
 import router from "./routes/routes.mjs";
 import dotenv from "dotenv";
+import { drizzle } from "drizzle-orm/libsql";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.BACKEND_PORT || 8080;
+const db = drizzle(process.env.DB_FILE_NAME);
 
 app.use("/", router);
 app.use(express.json());
