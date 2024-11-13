1917a1826200701dc683007d96622f275200b1f2
msilis <mikssilis@gmail.com>
2024-11-13 22:06:38 +0000

    [WIP] add family option when adding student

--- manage_frontend/app/routes/_layout.students.add.tsx
+++ manage_frontend/app/routes/_layout.students.add.tsx
@@ -1,18 +1,21 @@
-import { Form, Link } from "@remix-run/react";
+import { Form, Link, useLoaderData } from "@remix-run/react";
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
-import { addStudent } from "~/data/data";
+import { addStudent, getFamilies } from "~/data/data";
+import { FamilyRecord } from "~/types/types";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const body = await request.formData();
   const firstName = body.get("first_name");
   const lastName = body.get("last_name");
   const birthDate = body.get("birthdate");
+  const family = body.get("family");
 
   if (
     typeof firstName !== "string" ||
     typeof lastName !== "string" ||
-    typeof birthDate !== "string"
+    typeof birthDate !== "string" ||
+    typeof family !== "number"
   ) {
     throw new Error("Invalid form data");
   }
@@ -21,12 +24,19 @@ export const action = async ({ request }: ActionFunctionArgs) => {
     first_name: firstName,
     last_name: lastName,
     birthdate: birthDate,
+    family_id: family,
   });
 
   return redirect("/students");
 };
 
+export const loader = async () => {
+  return await getFamilies();
+};
+
 const AddStudent = () => {
+  const familyLastNames = useLoaderData<typeof loader>();
+
   return (
     <div>
       <Link to={"/students"}>
@@ -52,6 +62,21 @@ const AddStudent = () => {
           type="date"
           className="input input-bordered w-full max-w-xs"
         />
+        <label htmlFor="family">Family</label>
+        <select
+          name="family"
+          className="select select-bordered w-full max-w-xs"
+        >
+          <option value="">Choose a family</option>
+          {familyLastNames.map((family: FamilyRecord) => {
+            console.log(family.family_id);
+            return (
+              <option value={family.family_id} key={family.family_last_name}>
+                {family.family_last_name}
+              </option>
+            );
+          })}
+        </select>
         <button type="submit" className="btn-neutral btn-active w-fit p-3">
           Add Student
         </button>
