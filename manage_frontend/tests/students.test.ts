8b3db4023a5ddee539f3154c63031046ba1d3d04
msilis <mikssilis@gmail.com>
2025-03-14 21:05:14 +0000

    student page tests

---
+++ manage_frontend/tests/students.test.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "@playwright/test";
+
+test("Students page", async ({ page }) => {
+  await page.goto("/students");
+  await expect(page.getByText(/Students/)).toBeVisible();
+  await expect(page.getByText(/First Name/)).toBeVisible();
+
+  const addStudentButton = page.getByRole("button", { name: /Add Student/ });
+  await addStudentButton.click();
+  await expect(page).toHaveURL("/students/add");
+});
