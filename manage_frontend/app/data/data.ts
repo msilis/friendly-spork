05a6085bfc103a009430e5a07290d0e8051c1805
msilis <mikssilis@gmail.com>
2025-03-23 20:59:42 +0000

    use url variable

--- manage_frontend/app/data/data.ts
+++ manage_frontend/app/data/data.ts
@@ -8,22 +8,26 @@ import {
   TransactionRecord,
 } from "~/types/types";
 
+const BACKEND_URL = process.env.MANAGE_BACKEND
+  ? process.env.MANAGE_BACKEND
+  : "http://localhost3000";
+
 export const getStudents = async () => {
   console.log(
     typeof window === "undefined",
     "typeof window is undefined from get all students"
   );
 
-  const allStudents = await fetch(
-    `${process.env.MANAGE_BACKEND}/students`
-  ).then((response) => response.json());
+  const allStudents = await fetch(`${BACKEND_URL}/students`).then((response) =>
+    response.json()
+  );
   return allStudents;
 };
 
 export const getStudent = async (params: string | undefined) => {
   try {
     const getStudent = await fetch(
-      `${process.env.MANAGE_BACKEND}/students/${params}/edit`
+      `${BACKEND_URL}/students/${params}/edit`
     ).then((response) => response.json());
     return getStudent;
   } catch (error) {
@@ -33,42 +37,26 @@ export const getStudent = async (params: string | undefined) => {
 
 export const addStudent = async (data: StudentRecord) => {
   try {
-    const addStudent = await fetch(
-      `${process.env.MANAGE_BACKEND}/students/add`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    ).then((response) => response.json());
+    const addStudent = await fetch(`${BACKEND_URL}/students/add`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
     return addStudent;
   } catch (error) {
     console.error(error, "Error adding student");
   }
 };
-console.log(typeof window === "undefined", "typeof window is undefined");
-// console.log(process.env.MANAGE_BACKEND, "from env");
-console.log(
-  typeof window === "undefined"
-    ? process.env.MANAGE_BACKEND
-    : window.ENV.MANAGE_BACKEND,
-  "checkin the env"
-);
 
 export const updateStudent = async (
   data: StudentRecord,
   params: string | undefined
 ) => {
-  console.log(
-    typeof window === "undefined",
-    "typeof window is undefined, from update"
-  );
-
   try {
     const updateStudent = await fetch(
-      `${process.env.MANAGE_BACKEND}/students/${params}/edit`,
+      `${BACKEND_URL}/students/${params}/edit`,
       {
         method: "POST",
         headers: {
@@ -86,7 +74,7 @@ export const updateStudent = async (
 export const deleteStudent = async (studentId: number | undefined) => {
   if (studentId) {
     const deleteStudent = await fetch(
-      `${process.env.MANAGE_BACKEND}/students/${studentId}/delete`,
+      `${BACKEND_URL}/students/${studentId}/delete`,
       {
         method: "DELETE",
         headers: {
@@ -101,31 +89,28 @@ export const deleteStudent = async (studentId: number | undefined) => {
 };
 
 export const getFamilies = async () => {
-  const allFamilies = await fetch(
-    `${process.env.MANAGE_BACKEND}/families`
-  ).then((response) => response.json());
+  const allFamilies = await fetch(`${BACKEND_URL}/families`).then((response) =>
+    response.json()
+  );
   return allFamilies;
 };
 
 export const getFamily = async (params: string | undefined | null) => {
-  const family = await fetch(
-    `${process.env.MANAGE_BACKEND}/families/${params}`
-  ).then((response) => response.json());
+  const family = await fetch(`${BACKEND_URL}/families/${params}`).then(
+    (response) => response.json()
+  );
   return family;
 };
 
 export const addFamily = async (data: FamilyRecord) => {
   try {
-    const addFamily = await fetch(
-      `${process.env.MANAGE_BACKEND}/families/add`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    ).then((response) => response.json());
+    const addFamily = await fetch(`${BACKEND_URL}/families/add`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
     return addFamily;
   } catch (error) {
     console.error(error, "Error adding family");
@@ -137,16 +122,13 @@ export const updateFamily = async (
   id: string | undefined
 ) => {
   try {
-    const updateTeacher = await fetch(
-      `${process.env.MANAGE_BACKEND}/families/${id}/edit`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    ).then((response) => response.json());
+    const updateTeacher = await fetch(`${BACKEND_URL}/families/${id}/edit`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
     return updateTeacher;
   } catch (error) {
     console.error(error, "There was an error updating the family record.");
@@ -154,38 +136,35 @@ export const updateFamily = async (
 };
 
 export const getTeachers = async () => {
-  const allTeachers = await fetch(
-    `${process.env.MANAGE_BACKEND}/teachers`
-  ).then((response) => response.json());
+  const allTeachers = await fetch(`${BACKEND_URL}/teachers`).then((response) =>
+    response.json()
+  );
   return allTeachers;
 };
 
 export const getAccompanists = async () => {
-  const accompanists = await fetch(
-    `${process.env.MANAGE_BACKEND}/accompanists`
-  ).then((response) => response.json());
+  const accompanists = await fetch(`${BACKEND_URL}/accompanists`).then(
+    (response) => response.json()
+  );
   return accompanists;
 };
 
 export const getTeacher = async (params: string | undefined) => {
-  const teacher = await fetch(
-    `${process.env.MANAGE_BACKEND}/teachers/${params}/edit`
-  ).then((response) => response.json());
+  const teacher = await fetch(`${BACKEND_URL}/teachers/${params}/edit`).then(
+    (response) => response.json()
+  );
   return teacher;
 };
 
 export const addTeacher = async (data: TeacherRecord) => {
   try {
-    const addTeacher = await fetch(
-      `${process.env.MANAGE_BACKEND}/teachers/add`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    ).then((response) => response.json());
+    const addTeacher = await fetch(`${BACKEND_URL}/teachers/add`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
     return addTeacher;
   } catch (error) {
     console.error(error, "Error adding teacher");
@@ -198,7 +177,7 @@ export const updateTeacher = async (
 ) => {
   try {
     const updateTeacher = await fetch(
-      `${process.env.MANAGE_BACKEND}/teachers/${params}/edit`,
+      `${BACKEND_URL}/teachers/${params}/edit`,
       {
         method: "POST",
         headers: {
@@ -214,22 +193,22 @@ export const updateTeacher = async (
 };
 
 export const getClasses = async () => {
-  const allClasses = await fetch(`${process.env.MANAGE_BACKEND}/classes`).then(
-    (response) => response.json()
+  const allClasses = await fetch(`${BACKEND_URL}/classes`).then((response) =>
+    response.json()
   );
   return allClasses;
 };
 
 export const getClass = async (params: string | undefined) => {
-  const getClass = await fetch(
-    `${process.env.MANAGE_BACKEND}/classes/${params}/edit`
-  ).then((response) => response.json());
+  const getClass = await fetch(`${BACKEND_URL}/classes/${params}/edit`).then(
+    (response) => response.json()
+  );
   return getClass;
 };
 
 export const addClass = async (data: ClassRecord) => {
   try {
-    const addClass = await fetch(`${process.env.MANAGE_BACKEND}/classes/add`, {
+    const addClass = await fetch(`${BACKEND_URL}/classes/add`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -247,16 +226,13 @@ export const updateClass = async (
   params: string | undefined
 ) => {
   try {
-    const updateClass = await fetch(
-      `${process.env.MANAGE_BACKEND}/classes/${params}/edit`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    ).then((response) => response.json());
+    const updateClass = await fetch(`${BACKEND_URL}/classes/${params}/edit`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
     return updateClass;
   } catch (error) {
     console.error(error, "Error updating class");
@@ -266,7 +242,7 @@ export const updateClass = async (
 export const findStudentInClass = async (params: string | undefined) => {
   try {
     const findStudent = await fetch(
-      `${process.env.MANAGE_BACKEND}/classes/getstudent/${params}`,
+      `${BACKEND_URL}/classes/getstudent/${params}`,
       {
         method: "POST",
         headers: {
@@ -283,7 +259,7 @@ export const findStudentInClass = async (params: string | undefined) => {
 export const getFamilyTransactions = async (params: string | undefined) => {
   try {
     const findTransactions = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/${params}`,
+      `${BACKEND_URL}/transactions/${params}`,
       {
         method: "GET",
         headers: {
@@ -300,7 +276,7 @@ export const getFamilyTransactions = async (params: string | undefined) => {
 export const getTransaction = async (params: string | undefined) => {
   try {
     const findTransaction = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/get/${params}`,
+      `${BACKEND_URL}/transactions/get/${params}`,
       {
         method: "GET",
         headers: {
@@ -316,16 +292,13 @@ export const getTransaction = async (params: string | undefined) => {
 
 export const saveTransaction = async (data: TransactionRecord) => {
   try {
-    const saveTransaction = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/save`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    ).then((response) => response.json());
+    const saveTransaction = await fetch(`${BACKEND_URL}/transactions/save`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
     return saveTransaction;
   } catch (error) {
     console.error("There was an error saving the transaction: ", error);
@@ -338,7 +311,7 @@ export const updateTransaction = async (data: TransactionUpdateType) => {
   const id = data.id;
   try {
     const updateTransaction = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/get/${Number(id)}`,
+      `${BACKEND_URL}/transactions/get/${Number(id)}`,
       {
         method: "POST",
         headers: {
@@ -355,7 +328,7 @@ export const updateTransaction = async (data: TransactionUpdateType) => {
 
 export const deleteTransaction = async (params: number | undefined) => {
   const deleteTransaction = await fetch(
-    `${process.env.MANAGE_BACKEND}/transactions/get/${params}/delete`,
+    `${BACKEND_URL}/transactions/get/${params}/delete`,
     {
       method: "DELETE",
       headers: {
@@ -378,7 +351,7 @@ export const getTransactionsForInvoice = async (data: DateRangeType) => {
     throw new Error("Getting transactions for invoice needs an account id");
   try {
     const getTransactionsForInvoice = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/range`,
+      `${BACKEND_URL}/transactions/range`,
       {
         method: "POST",
         headers: {
@@ -404,7 +377,7 @@ export const getTransactionsFromInvoice = async (
   }
   try {
     const invoiceTransactions = await fetch(
-      `http://localhost:3000/transactions/invoices/recreate/${invoiceId}`
+      `$${BACKEND_URL}/transactions/invoices/recreate/${invoiceId}`
     ).then((response) => response.json());
     return invoiceTransactions;
   } catch (error) {
@@ -415,7 +388,7 @@ export const getTransactionsFromInvoice = async (
 export const getLastInvoice = async () => {
   try {
     const retreiveLastInvoice = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/invoices/get_last_record`,
+      `${BACKEND_URL}/transactions/invoices/get_last_record`,
       {
         method: "GET",
         headers: {
@@ -432,7 +405,7 @@ export const getLastInvoice = async () => {
 export const saveInvoice = async (data: SaveDataType) => {
   try {
     const saveInvoice = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/invoices/save`,
+      `${BACKEND_URL}/transactions/invoices/save`,
       {
         method: "POST",
         headers: {
@@ -451,7 +424,7 @@ export const updateInvoice = async (data: InvoiceRecord | undefined) => {
   const invoiceId = data?.invoice_id;
   try {
     const updateInvoiceStatus = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/invoices/update/${invoiceId}`,
+      `${BACKEND_URL}/transactions/invoices/update/${invoiceId}`,
       {
         method: "POST",
         headers: {
@@ -469,7 +442,7 @@ export const updateInvoice = async (data: InvoiceRecord | undefined) => {
 export const getAllInvoices = async () => {
   try {
     const retreiveAllInvoices = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/invoices/get`,
+      `${BACKEND_URL}/transactions/invoices/get`,
       {
         method: "GET",
         headers: {
@@ -486,7 +459,7 @@ export const getAllInvoices = async () => {
 export const getInvoiceForFamily = async (familyId: string | undefined) => {
   try {
     const retreiveFamilyInvoices = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/invoices/get/family/${familyId}`,
+      `${BACKEND_URL}/transactions/invoices/get/family/${familyId}`,
       {
         method: "POST",
         headers: {
@@ -503,7 +476,7 @@ export const getInvoiceForFamily = async (familyId: string | undefined) => {
 export const getInvoice = async (params: number) => {
   try {
     const retreiveInvoice = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/invoices/get/${params}`,
+      `${BACKEND_URL}/transactions/invoices/get/${params}`,
       {
         method: "POST",
         headers: {
@@ -520,7 +493,7 @@ export const getInvoice = async (params: number) => {
 export const deleteInvoice = async (invoiceToDelete: number) => {
   try {
     const deleteInvoice = await fetch(
-      `${process.env.MANAGE_BACKEND}/transactions/invoices/${invoiceToDelete}/delete`,
+      `${BACKEND_URL}/transactions/invoices/${invoiceToDelete}/delete`,
       {
         method: "DELETE",
         headers: {
@@ -535,15 +508,15 @@ export const deleteInvoice = async (invoiceToDelete: number) => {
 };
 
 export const getSettings = async () => {
-  const settings = await fetch(`${process.env.MANAGE_BACKEND}/settings`).then(
-    (response) => response.json()
+  const settings = await fetch(`${BACKEND_URL}/settings`).then((response) =>
+    response.json()
   );
   return settings;
 };
 
 export const saveSettings = async (data: unknown) => {
   try {
-    const saveSettings = await fetch(`${process.env.MANAGE_BACKEND}/settings`, {
+    const saveSettings = await fetch(`${BACKEND_URL}/settings`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
