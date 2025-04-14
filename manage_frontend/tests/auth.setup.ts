c9c16dcbabcdf502ea7b9ad480706d07f819d8f6
msilis <mikssilis@gmail.com>
2025-04-14 23:15:05 +0100

    put it all back

--- manage_frontend/tests/auth.setup.ts
+++ manage_frontend/tests/auth.setup.ts
@@ -9,15 +9,14 @@ const authFile = path.join(__dirname, "../playwright/.auth/user.json");
 setup("authenticate", async ({ page }) => {
   const testEmail = process.env.PLAYWRIGHT_TEST_EMAIL;
   const testPassword = process.env.PLAYWRIGHT_TEST_PASSWORD;
-  console.log(testEmail, "testEmail");
-  console.log(testPassword, "testPassword");
+
   if (!testEmail || !testPassword) {
     console.warn("Missing test email or password");
     return;
   }
   await page.goto("/login");
-  await page.getByTestId("email-input").fill("testemail@email.com");
-  await page.getByTestId("password-input").fill("testPassword123");
+  await page.getByTestId("email-input").fill(testEmail);
+  await page.getByTestId("password-input").fill(testPassword);
   await page.getByRole("button", { name: "Login" }).click();
 
   await page.waitForURL("/dashboard");
