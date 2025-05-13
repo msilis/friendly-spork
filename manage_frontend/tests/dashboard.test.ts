63645a0bd62ef9d915059f92f6ae92bb333c67c8
msilis <mikssilis@gmail.com>
2025-05-13 23:22:48 +0100

    remove test for invoice

--- manage_frontend/tests/dashboard.test.ts
+++ manage_frontend/tests/dashboard.test.ts
@@ -4,7 +4,6 @@ test("Dashboard", async ({ page }) => {
   await page.goto("/dashboard");
   const dashboardTitle = page.getByTestId("dashboardContainer");
   await expect(dashboardTitle).toBeVisible();
-  await expect(page.getByText("Total outstanding invoices")).toBeVisible();
   await expect(page.getByText("Manage")).toBeVisible();
   const menuButton = page.getByTestId("main-menu");
   await menuButton.click();
